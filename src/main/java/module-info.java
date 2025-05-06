318f898e6a51c06672490ce89b33ada2ffb26b76
Nathan Page <200609542@student.georgianc.on.ca>
2025-05-06 13:33:40 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.comp1011spring2025_nathan_page_200609542 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.comp1011spring2025_nathan_page_200609542 to javafx.fxml;
+    exports com.example.comp1011spring2025_nathan_page_200609542;
+}
