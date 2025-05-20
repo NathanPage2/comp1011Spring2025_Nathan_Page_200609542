84aef5606e5ab50605a4e655cab7078685d33c9f
Nathan Page <200609542@student.georgianc.on.ca>
2025-05-20 13:36:36 -0400

    Updating the files to be more standard with the class Lab 1 on bottom of Camera Controller

--- src/main/java/com/example/comp1011spring2025_nathan_page_200609542/CameraController.java
+++ src/main/java/com/example/comp1011spring2025_nathan_page_200609542/CameraController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+
 public class CameraController {
     @FXML
     private ComboBox<String> brandList;
