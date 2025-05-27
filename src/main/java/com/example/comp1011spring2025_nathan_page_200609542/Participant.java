3a97ef5798d0c5e6a43530ffd383847562332459
Nathan Page <200609542@student.georgianc.on.ca>
2025-05-27 15:52:29 -0400

    Week 4

---
+++ src/main/java/com/example/comp1011spring2025_nathan_page_200609542/Participant.java
@@ -0,0 +1,46 @@
+package com.example.comp1011spring2025_nathan_page_200609542;
+
+import java.util.ArrayList;
+
+
+public class Participant {
+    private String name;
+    private String avatar;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+
+    public Participant(String name, String avatar) {
+        setName(name);
+        setAvatar(avatar);
+    }
+    public static ArrayList<String> getAllAvatars(){
+        ArrayList<String> avatars = new ArrayList<>();
+        avatars.add("avatar1.png");
+        avatars.add("avatar2.png");
+        avatars.add("avatar3.png");
+        return avatars;
+    }
+    public static ArrayList<String> getAllNames() {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Alex");
+        names.add("Bob");
+        names.add("Charles");
+        names.add("David");
+        return names;
+    }
+
+}
