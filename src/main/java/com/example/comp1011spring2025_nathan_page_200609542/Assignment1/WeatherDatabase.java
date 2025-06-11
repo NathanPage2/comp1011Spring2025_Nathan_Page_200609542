a35cc515d49d5fc9e9029d7790ab5c51c8d183d7
Nathan Page <200609542@student.georgianc.on.ca>
2025-06-11 12:56:56 -0400

    Had to brute force attach the SQL so it would work and tested it via prepared statement and added blank controllers and views for later as I will need both for the two charts.

--- src/main/java/com/example/comp1011spring2025_nathan_page_200609542/Assignment1/WeatherDatabase.java
+++ src/main/java/com/example/comp1011spring2025_nathan_page_200609542/Assignment1/WeatherDatabase.java
@@ -4,11 +4,30 @@ public class WeatherDatabase {
     public static void main(String[] args){
 
         try{
-        Connection connection = DriverManager.getConnection("jdbc:mysql://database-1.clcsm88w2wu1.us-east-2.rds.amazonaws.com:3306/COMP1011;user=NathanPage;password=Jarwars89;");
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://database-1.clcsm88w2wu1.us-east-2.rds.amazonaws.com:3306/COMP1011",
+                    "NathanPage",
+                    "Jarwars89"
+                    //Had to force it through with cloudshell commands
+            );
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO weatherApp (city,timeDay, temperature, weather, specialNote) VALUES (?,?,?,?,?)");
+            System.out.println("Test");
+
+            Time currentTime = new Time(System.currentTimeMillis());
+            //Gets the currant time which is nice
+            preparedStatement.setString(1, "Sutton");
+            preparedStatement.setTime(2, currentTime);
+            preparedStatement.setDouble(3, 22);
+            preparedStatement.setString(4, "Sunny");
+            preparedStatement.setString(5, "Weather is very nice out");
+
+            preparedStatement.execute();
+            //Executed successfully
         }
 
         catch(SQLException e){
             e.printStackTrace();
+            //Placeholder catch to simply not annoy me
         }
 
 
---
+++ src/main/java/com/example/comp1011spring2025_nathan_page_200609542/Assignment1/WeatherTableController.java
@@ -0,0 +1,4 @@
+package com.example.comp1011spring2025_nathan_page_200609542.Assignment1;
+
+public class WeatherTableController {
+}
