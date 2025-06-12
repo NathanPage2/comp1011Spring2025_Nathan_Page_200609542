2dfe0d674ead9dee547bb91392d4e506451c659e
Nathan Page <200609542@student.georgianc.on.ca>
2025-06-12 14:29:46 -0400

    Basic set-up for controller and had to go back and give the table view an id because i forgot to

--- src/main/java/com/example/comp1011spring2025_nathan_page_200609542/Assignment1/WeatherTableController.java
+++ src/main/java/com/example/comp1011spring2025_nathan_page_200609542/Assignment1/WeatherTableController.java
@@ -1,4 +1,44 @@
 package com.example.comp1011spring2025_nathan_page_200609542.Assignment1;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class WeatherTableController {
+    @FXML
+    private TableView<WeatherModel> tableView;
+    @FXML
+    private TableColumn<WeatherModel, Integer> idColumn;
+
+    @FXML
+    private TableColumn<WeatherModel, String> cityColumn;
+
+    @FXML
+    private TableColumn<WeatherModel, String> timeDayColumn;
+
+    @FXML
+    private TableColumn<WeatherModel, Double> tempertaureColumn;
+
+    @FXML
+    private TableColumn<WeatherModel, String> weatherColumn;
+
+    @FXML
+    private TableColumn<WeatherModel, String> specialNotesColumn;
+
+    public void initialize() {
+        idColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getId()).asObject());
+        cityColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCity()));
+        timeDayColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTimeDay().toString()));
+        tempertaureColumn.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getTemperature()).asObject());
+        weatherColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getWeather()));
+        specialNotesColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getSpecialNotes()));
+
+    }
+
+
+
 }
